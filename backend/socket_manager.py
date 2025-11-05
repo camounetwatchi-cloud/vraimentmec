44a43f95251aa43e9952822f93167f3f0cfe3472
camounetwatchi-cloud <camounetwatchi@gmail.com>
2025-11-05 11:43:31 +0100

    Update socket_manager.py

--- backend/socket_manager.py
+++ backend/socket_manager.py
@@ -255,6 +255,8 @@ self.last_move_time = now
                 self.save_to_database(result)
             
             return self.board.fen(), status, {
+                'white_time': self.white_time,
+'                black_time': self.black_time
                 'moves_count': len(self.moves_history),
                 'last_move': uci_move,
                 'result': result,
